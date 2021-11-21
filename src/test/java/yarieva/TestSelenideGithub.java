d1309b31ab83ba5dc67fd72b94b1b82a37d99f02
C1 <yarievakamila@gmail.com>
2021-11-22 02:29:50 +0300

    add open page SoftAssertions

--- src/test/java/yarieva/TestSelenideGithub.java
+++ src/test/java/yarieva/TestSelenideGithub.java
@@ -27,9 +27,26 @@ open("/selenide/selenide");
         $("#wiki-tab").click();
 
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+       $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
 
+        //- Откройте страницу SoftAssertions,
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
 
-sleep(500);
+        //проверьте что внутри есть пример кода для JUnit5
+        $("#pre div").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+sleep(1000);
 
     }
 
